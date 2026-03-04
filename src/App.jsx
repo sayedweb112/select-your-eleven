b46319bdacd7f444534dd04b7a6d420b0804ed0b
unknown <112sayedalsahab@gmail.com>
2026-03-04 14:26:06 +0600

    add more info

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,27 @@ import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
 const fetchPlayers = async () => {
   const res = await fetch("/players.json")
   return res.json()
- }
+}
 
 function App() {
 
-const playersPromise= fetchPlayers()
+  const playersPromise = fetchPlayers()
   return (
     <>
 
       <Navbar></Navbar>
-      
+      <div className='max-w-[1200px] mx-auto flex justify-between items-center '>
+        <h1 className='font-bold text-2xl'>Available Players</h1>
+        <div className='font-bold'>
+          <button className='py-3 px-4 border-1 border-gray-400 rounded-l-2xl boder-r-0 bg-green-300'>Available</button>
+          <button className='py-3 px-4 border-1 border-l-0 border-gray-400 rounded-r-2xl'>Selected <span>(0)</span></button>
+        </div>
+      </div>
+     
       <Suspense fallback={<h3 className="max-w-[1200px] mx-auto"><span className="loading loading-spinner loading-xl"></span></h3>}>
         <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
       </Suspense>
-      {/* <SelectedPlayers></SelectedPlayers> */}
+      <SelectedPlayers></SelectedPlayers>
     </>
   )
 }
